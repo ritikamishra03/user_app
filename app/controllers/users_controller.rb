3e57a2516e278c952f89f4cf6a6132bf31ab0294
ritika mishra <mritika233@gmail.com>
2024-06-16 12:32:52 +0530

    changes added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,12 @@ class UsersController < ApplicationController
   end
 
   def create
+   @user=User.new(user_params)
+    if @user.save
+      redirect_to '/users', notice: 'user was successfully created'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +26,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
